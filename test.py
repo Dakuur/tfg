d1806ad4f8ea2e4ada083ab9b813373f0c7c7d3d
David <davidmormas@gmail.com>
2026-02-18 18:18:25 +0100

    prova gpu

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ model = GATWithPooling().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 torch.cuda.synchronize()
-for step in range(20):
+for step in range(200):
     optimizer.zero_grad()
     out = model(data.x, data.edge_index, data.batch)
     loss = out.mean()
